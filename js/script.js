5d51d0da0702373b1c65ece016ee17ac95032071
Gustavo Chitangua <gchitangua@gmail.com>
2024-04-15 21:42:49 +0100

    first-commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const btnDisplay = document.querySelector(".btn-add--display");
+const btnAdd = document.querySelector(".btn-add--add");
+const btnSubtract = document.querySelector(".btn-add--subtract");
+const btnDivide = document.querySelector(".btn-add--divide");
+const btnMultiply = document.querySelector(".btn-add--multiply");
+
+const buttonElements = document.querySelectorAll(".btn");
+
+buttonElements.forEach((el) => {
+  el.addEventListener("click", function (e) {
+    const clicked = e.target;
+
+    let num = parseInt(btnDisplay.innerText);
+
+    if (clicked === btnAdd) {
+      num === -1
+        ? (btnDisplay.innerText = num + 2)
+        : (btnDisplay.innerText = num + 1);
+    } else if (clicked === btnSubtract) {
+      btnDisplay.innerText = num - 2;
+    } else if (clicked === btnMultiply) {
+      btnDisplay.innerText = num * 2;
+    } else if (clicked === btnDivide) {
+      num === 1
+        ? (btnDisplay.innerText = 2 / 2)
+        : (btnDisplay.innerText = num / 2);
+
+      num === -1
+        ? (btnDisplay.innerText = -2 / 2)
+        : (btnDisplay.innerText = num / 2);
+    }
+  });
+});
+
+// btnDisplay.addEventListener("click", function () {
+//   let num = document.getElementById("btn-add").innerText;
+//   document.getElementById("btn-add").innerText = parseInt(num) - 2;
+// });
